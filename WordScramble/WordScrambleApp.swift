0272640c4d3811e5e60ee879dfb2988447f39298
Rob Ranf <rob@emiyaconsulting.com>
2023-03-03 12:25:45 -0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Rob Ranf on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
